6b226a0fccb155123b72987114128445e0c61a35
Oliver Oquist <o.oequist@sculpt.digital>
2024-03-15 12:34:58 +0000

    styling to navbar, also button for mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import burgerMenu from "../assets/burger_menu.png" ;
+import closeX from "../assets/close_x.png";
+
+
 
 const Navbar = () => {
+    // <p className="navbar__link" href="#">News</p>
+    // <p className="navbar__link" href="#">Game Info</p>
+    // <p className="navbar__link" href="#">Beta</p>
+    // <p className="navbar__link" href="#">Account</p>
+    // <p className="navbar__link" href="#">Play Now</p>
+
+    const links = [
+        {id: 0, name: 'News' },
+        {id: 1, name: 'Game Info' },
+        {id: 2, name: 'Beta' },
+        {id: 3, name: 'Account' },
+        {id: 4, name: 'Play Now' },
+    ];
+
+    const [openMenu, setOpenMenu] = useState(false);
+    
+    const handleBurgerMenu = () => {
+      setOpenMenu((prev) => !prev);
+    };
+    
+    // const closeBurgerMenu = () => {
+    //   setOpen(false);
+    // };
+
     return (
         <div className="navbar">
             <div className="navbar__items">
-                <p>IKOL</p>
-                <div>    
-                    <a href="#">News</a>
-                    <a href="#">Game Info</a>
-                    <a href="#">Beta</a>
-                    <a href="#">Account</a>
-                    <a href="#">Play Now</a>
+                <p className="navbar__logo">IKOL</p>
+                <div className="navbar__links">
+                    {links.map((link) => (
+                        <p key={link.id} className="navbar__link">{link.name}</p>
+                    ))}
                 </div>
+                {openMenu ? (
+                    <div className="navbar__burger-close" >
+                        <img src={closeX} onClick={handleBurgerMenu} alt="close"></img>
+                    </div>
+                ) : (
+                <div className="navbar__burger">
+                    <img src={burgerMenu} onClick={handleBurgerMenu} alt="burgerMenu"></img>
+                </div>
+                )}
             </div>
         </div>
     )
