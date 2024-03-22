411cbe078aac86d61a45e688c59e2d0a81a51a5c
Olipolite <oliver.oquist3d@gmail.com>
2024-03-22 11:52:27 +0000

    new colours for block, brighter overall,

--- src/components/BetaBlock.jsx
+++ src/components/BetaBlock.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Dragon from "../assets/dragon.jpg";
 
 const BetaBlock = () => {
     return (
-        <div className="beta">
-            <p className="beta__para">Hello World!</p>
+        <div className="beta-block">
+            <div className="beta-block__container">
+                <div className="beta-block__background">
+                    <p>Hello World</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/BetaPage.jsx
+++ src/pages/BetaPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Dragon from "../assets/dragon.jpg";
 
 const BetaPage = () => {
     return (
         <div className="beta-page">
             <div className="beta-page__container">
+                <div className="beta-page__background">
+                    <img className="beta-page__dragon" src={Dragon} alt="Dragon"></img>
+                </div>
                 <h1>Hello World</h1>
             </div>
         </div>
