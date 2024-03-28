f6eda156a87940cd74cec8f4e9d177ded27b6788
Olipolite <oliver.oquist3d@gmail.com>
2024-03-28 15:50:02 +0000

    Now has working border animations inside of beta block

--- src/components/BetaBlock.jsx
+++ src/components/BetaBlock.jsx
@@ -6,20 +6,40 @@ const BetaBlock = () => {
     const boxBorder = useRef(null);
     const [borderVisible, setBorderVisible] = useState(false);
 
+    const boxBorderBottom = useRef(null);
+    const [borderVisibleBottom, setBorderVisibleBottom] = useState(false);
+
     const animateBorderFill = () => {
         setBorderVisible(true);
+        setBorderVisibleBottom(true);
 
-        gsap.fromTo(
+        let tl = gsap.timeline();
+    
+        tl.fromTo(
             boxBorder.current,
-            { width: 0, },
-            {   
-                ease: 'none',
-                width: '100%',
-                duration: 4,
-                borderTop: '1px solid rgb(241, 145, 1)',
-            }
+            { width: 0 },
+            { ease: 'power3.out', width: '100%', duration: 1, borderTop: '1px solid rgb(241, 145, 1)' }
+        );
+        tl.fromTo(
+            boxBorder.current,
+            { height: 0 },
+            { ease: 'power3.out', height: '100%', duration: 1, borderRight: '1px solid rgb(241, 145, 1)' }
+        );
+        tl.fromTo(
+            boxBorderBottom.current,
+            { left: '100%', width: 0, }, 
+            { ease: 'power3.out', left: 0, width: '100%', duration: 1, borderBottom: '1px solid rgb(241, 145, 1)' }
+        ); 
+        tl.fromTo(
+            boxBorderBottom.current,
+            { height: 0, top: '100%' }, 
+            { ease: 'power3.out', height: '100%', top: 0, duration: 1, borderLeft: '1px solid rgb(241, 145, 1)' },
         );
     };
+
+
+
+
 
     return (
         <div className="beta-block">
@@ -30,6 +50,7 @@ const BetaBlock = () => {
                     <p className="beta-block__paragraph">Dolor qui ea laborum est velit ipsum exercitation ipsum adipisicing.Consequat ex amet ex aute est laboris duis esse.</p>
                     <button className="btn beta-block__button" onClick={animateBorderFill}>BETA</button>
                     <div style={{ display: borderVisible ? 'block' : 'none' }} ref={boxBorder} className="beta-block__content-border"></div>
+                    <div style={{ display: borderVisibleBottom ? 'block' : 'none' }} ref={boxBorderBottom} className="beta-block__content-border-bottom"></div>
                 </div>
             </div>
         </div>
