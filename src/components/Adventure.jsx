a41a36362e0c5f71a5990d8531303203839b550b
Olipolite <oliver.oquist3d@gmail.com>
2024-04-10 17:19:14 +0100

    added content to adventure component

--- src/components/Adventure.jsx
+++ src/components/Adventure.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import AdventureOne from "../assets/adventure1.png";
+import AdventureTwo from "../assets/adventure2.png";
+import AdventureThree from "../assets/Adventure3.jpeg";
+import AdventureFour from "../assets/adventure4.png";
 
 const Adventure = () => {
 
     return (
         <div className="adventure">
             <div className="adventure__container">
-                <h1>Adventure awaits you.</h1>
+                <div className="adventure__text">
+                    <p className="adventure__title">ADVENTURE</p>
+                    <p className="adventure__sub">AWAITS</p>
+                    <button className="adventure__btn btn">PLAY NOW</button>
+                </div>
+                <img className="adventure_image" src={AdventureFour} alt="Warrior"></img>
             </div>
         </div>
     )
 }
 
 export default Adventure;
-
