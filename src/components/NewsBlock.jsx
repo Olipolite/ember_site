c71eff77158895041669da75c1d8845f7b5c22c4
Olipolite <oliver.oquist3d@gmail.com>
2024-04-26 15:32:04 +0100

    New animation for news title and scroll based animation card

--- src/components/NewsBlock.jsx
+++ src/components/NewsBlock.jsx
@@ -3,7 +3,7 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 // images
-import Placeholder from "../assets/portraits_classes/samurai_cool.png";
+import Placeholder from '../assets/portraits_classes/samurai_cool.png';
 
 const NewsBlock = () => {
     const cardOneRef = useRef(null);
@@ -12,106 +12,119 @@ const NewsBlock = () => {
 
     const newsBlockTitle = useRef(null);
 
+    // //Title Animation
+    // useEffect(() => {
+    //     gsap.registerPlugin(ScrollTrigger);
+    //     gsap.fromTo(
+    //         newsBlockTitle.current,
+    //         {
+    //             opacity: 0,
+    //             y: -25,
+    //         },
+    //         {
+    //             y: 0,
+    //             opacity: 1,
+    //             duration: 1,
+    //             scrollTrigger: {
+    //                 trigger: newsBlockTitle.current,
+    //                 start: 'top 80%',
+    //                 toggleActions: 'play none none none',
+    //             },
+    //         }
+    //     );
+    // }, []);
+
+    //New Title Animation
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger);
         gsap.fromTo(
             newsBlockTitle.current,
             {
-                opacity: 0,
-                y: -25,
+                x: -500,
             },
             {
-                y: 0,
-                opacity: 1,
+                x: 0,
                 duration: 1,
                 scrollTrigger: {
                     trigger: newsBlockTitle.current,
                     start: 'top 80%',
                     toggleActions: 'play none none none',
+                    ease: 'linear',
+                },
+                onComplete: () => {
+                    gsap.set(newsBlockTitle.current, { backgroundColor: 'transparent' });
                 },
             }
         );
-    }, []);
+    });
 
+    // Card Animation
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger);
-        
+
         const animations = [
             {
                 element: cardOneRef.current,
                 props: {
-                    opacity: 0,
-                    y: -100,
+                    y: 350,
+                    start: 'top 120%',
+                    end: '+=500',
+                    delay: 0,
                 },
             },
             {
                 element: cardTwoRef.current,
                 props: {
-                    opacity: 0,
-                    y: -100,
+                    y: 350,
+                    start: 'top 120%',
+                    end: '+=500',
+                    delay: 0,
                 },
             },
         ];
-    
+
+        const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+        if (mediaQuery.matches) {
+            animations.forEach((animation) => {
+                animation.props.start = 'top 150%';
+                animation.props.end = 'bottom 140%';
+            });
+        }
+
         animations.forEach((animation, index) => {
-            gsap.fromTo(
-                animation.element,
-                animation.props,
-                {
-                    y: 0,
-                    opacity: 1,
-                    duration: 1,
-                    scrollTrigger: {
-                        trigger: animation.element,
-                        start: 'top 80%',
-                        toggleActions: 'play none none none',
-                    },
-                    stagger: 0.4,
-                    delay: index * 0.2, // Add a delay based on index to stagger animations
-                }
-            );
+            gsap.fromTo(animation.element, animation.props, {
+                y: 0,
+                scrollTrigger: {
+                    trigger: animation.element,
+                    start: animation.props.start,
+                    end: animation.props.end,
+                    scrub: 1,
+                    ease: 'power4.inOut',
+                },
+                stagger: {
+                    each: 1,
+                    from: 'random',
+                },
+                delay: animation.props.delay,
+            });
         });
     }, []);
-    
-    const cardHoverAnimation = () => {
-        gsap.to(
-            cornerCutRef.current,
-            {   
-                ease: 'linear',
-                rotation: 180,
-                top: 0,
-                right: 0,
-                background: 'transparent',
-                duration: 0.5,
-            }
-        )
-    };
-
-    const leaveHoverAnimation = () => {
-        gsap.to(
-            cornerCutRef.current,
-            {
-                ease: 'linear',
-                rotation: 360,
-                top: '-25',
-                right: '-25',
-                background: 'white',
-                duration: 0.5,
-            }
-        )
-    };
 
     return (
         <div className="newsblock">
             <div className="newsblock__container">
                 <div className="newsblock__title-wrapper">
-                    <h1 className="newsblock__title" ref={newsBlockTitle}>NEWS</h1>
+                    <div className="newsblock__title-animation" ref={newsBlockTitle}></div>
+                    <h1 className="newsblock__title">
+                        NEWS
+                    </h1>
                     <div className="newsblock__part-underline"></div>
                 </div>
                 <div className="newsblock__card-wrapper">
-                    <div className="newsblock__card" ref={cardOneRef} onMouseEnter={cardHoverAnimation} onMouseLeave={leaveHoverAnimation}>
+                    <div className="newsblock__card" ref={cardOneRef}>
                         <img className="card__thumbnail" alt="" src={Placeholder}></img>
-                        <p className="card__paragraph">Commodo magna cupidatat do consequat consectetur incididunt ut. JORDEL CHAD</p>
+                        <p className="card__paragraph">Commodo magna cupidatat do consequat consectetur incididunt ut. Eiusmod aute incididunt non et anim ut id cupidatat commodo. Proident quis aliquip reprehenderit aliquip eiusmod quis fugiat aliquip officia irure occaecat officia. JORDEL CHAD</p>
                         <div className="card__border"></div>
                         <span className="card__corner-cut" ref={cornerCutRef}></span>
                     </div>
@@ -119,11 +132,12 @@ const NewsBlock = () => {
                         <img className="card__thumbnail" alt="" src={Placeholder}></img>
                         <p className="card__paragraph">Commodo magna cupidatat do consequat consectetur incididunt ut. PLEASE BUFF WARRIOR PLEASE // BASSE</p>
                         <div className="card__border"></div>
+                        <span className="card__corner-cut" ref={cornerCutRef}></span>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default NewsBlock;
